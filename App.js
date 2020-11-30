49a009ce3669ed8b2e9b5ae413773c0a84358431
Max Dayton <daytonmaximus@gmail.com>
2020-11-30 11:30:49 -0700

    initial setup

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import ScanBarcodeScreen from './screens/ScanBarcodeScreen';
+
+const Start = ({route, navigation}) => {
+  return (
+    <View style={styles.screen}>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Scan')}>
+        <Text style={styles.buttonText}>Scan Barcode</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Start">
+        <Stack.Screen name="Start" component={Start} />
+        <Stack.Screen name="Scan" component={ScanBarcodeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#e74c3c',
+    width: 200,
+    alignItems: 'center',
+    paddingVertical: 20,
+    marginBottom: 50,
+    borderRadius: 10
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18
   },
 });
